5fe28ded5de697e618b705946d5bb6a539bb8b04
carlxw <87335263+carlxw@users.noreply.github.com>
2023-02-22 15:35:20 -0500

    Push

---
+++ src/components/Alert.js
@@ -0,0 +1,13 @@
+import "../Alert.css";
+
+const Alert = () => {
+    return (
+        <div style={{ zIndex: 1 }}>
+            <div>
+                <p>Message here</p>
+            </div>
+        </div>
+    );
+}
+
+export default Alert;
