d84783f1a4f613e6d434b58ee207158b68b85293
carlxw <87335263+carlxw@users.noreply.github.com>
2023-03-04 16:36:17 -0500

    Update get_playlist_tracks.js

--- src/get_playlist_tracks.js
+++ src/get_playlist_tracks.js
@@ -2,28 +2,64 @@
 // https://developer.spotify.com/console/get-playlist-tracks/?playlist_id=&market=&fields=&limit=&offset=&additional_types=
 
 // playlist-read-private scope
-const token = "BQB4Z6s0ep1Pij8qTXlNkSd96tUdlqct418RKxEwuKdBMN07uYgH7_CmLLIw3pRyRzNfbpIzt9s3Mjgj9G_uuMOU3noJhNqqbmu2PyulSQl6UyNuEp2uuWBHPSRl1yRQs5GXwiwkmoS0cIiqQEcCVvim-OspZlGbQfEyW6G0xD-ohprdcJTcvFyks3mDzEY";
+const token = "BQBZbLk9u4rfLMmzPA1a5nTIgd18QRoaAnXO2L1VzADyFFcSmHoRaDmM_GcjRMtjwMZhxYLPiSKJ4N21BMQ1nT65PLtXYdw7nuvaHMUte55BNKiKQ5Ma7SbyLHywCalUngPTsxJWiFD9yVSkTN3Rt7bf_Ekp8UhlvAUTf4I0H7pEc3aul8HmtYH5BEnVHg0";
 
 const processLink = (str) => {
     str = str.replace("https://open.spotify.com/playlist/", "");
     return str.substring(0, str.indexOf("?"));
 }
-const link = processLink("https://open.spotify.com/playlist/2FmWFmEIH44B6y1ww7E47u?si=7d474e7536a14af5&pt=6d41266a39c65638961cda57002c3ed5");
 
-const data = fetch(`https://api.spotify.com/v1/playlists/${link}/tracks`, {
-    method: "GET",
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-    }
-}).then(res => res.json()).then(
-    data => {
-        for (let i = 0; i < data.items.length; i++) {
-            console.log(data.items[i].track.name); // Song name
-            console.log(data.items[i].track.album.artists[0].name); // Artist name
-            console.log(data.items[i].track.album.images[0].url); // Largest album cover
-            console.log();
+const getSpotifyPlaylist = async (link) => {
+    const data = await fetch(`https://api.spotify.com/v1/playlists/${link}/tracks`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
         }
+    }).then(res => res.json()).then(
+        data => {
+            return data;
+        }
+    ).catch(err => console.err(err));
+
+    const playlist = [];
+    for (let i = 0; i < data.items.length; i++) {
+        playlist.push({
+            "name": data.items[i].track.name,
+            "artist": data.items[i].track.album.artists[0].name,
+            "cover": data.items[i].track.album.images[0].url
+        })
     }
-);
+
+    return playlist;
+}
+
+// Returns the number of times needed to run fetch
+const getSpotifyPlaylistLength = (link) => {
+    const playlist = fetch(`https://api.spotify.com/v1/playlists/${link}/tracks`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    }).then(res => res.json()).then(
+        data => {
+            return data;
+        }
+    ).catch(err => console.err(err));
+
+    return Math.floor(playlist.total/100) + 1;
+}
+
+async function main() {
+    const link = processLink("https://open.spotify.com/playlist/4ERjA5BDMJ1cGt3URjbW6i?si=0c55f05f3f934d73&pt=d45541bf4495898d71e9f772524f389f");
+
+    const playlist = await getSpotifyPlaylist(link);
+
+    playlist.forEach((x) => {
+        console.log(x);
+    })
+}
+main();
