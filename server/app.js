72bea2c91b5b1cadb3117fc278c9fb80ad08d521
carlxw <87335263+carlxw@users.noreply.github.com>
2023-03-08 17:17:49 -0500

    Push

--- server/app.js
+++ server/app.js
@@ -46,21 +46,21 @@ app.get('/auth/callback', (req, res) => {
 	let authOptions = {
 		url: 'https://accounts.spotify.com/api/token',
 		form: {
-		code: code,
-		redirect_uri: spotify_redirect_uri,
-		grant_type: 'authorization_code'
+			code: code,
+			redirect_uri: spotify_redirect_uri,
+			grant_type: 'authorization_code'
 		},
 		headers: {
-		'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
-		'Content-Type' : 'application/x-www-form-urlencoded'
+			'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+			'Content-Type' : 'application/x-www-form-urlencoded'
 		},
 		json: true
 	};
 
 	request.post(authOptions, function(error, response, body) {
 		if (!error && response.statusCode === 200) {
-		access_token = body.access_token;
-		res.redirect('/')
+			access_token = body.access_token;
+			res.redirect('/')
 		}
 	});
 })
--- src/sections/Music.js
+++ src/sections/Music.js
@@ -1,52 +1,52 @@
 import { useEffect, useState } from "react";
 import MusicPlayer from "../components/MusicPlayer";
 import SpotifyLogin from "../components/SpotifyLogin";
-import image from "../album-cover.jpg";
+import SpotifyMusicPlayer from "../components/SpotifyMusicPlayer";
 
 const Music = () => {
-    const [url, setUrl] = useState("");
-    const [token, setToken] = useState("");
+    // const [url, setUrl] = useState("");
+    const [token, setToken] = useState('');
 
     useEffect(() => {
-        async function getToken() {
-            const res = await fetch("/auth/token");
-            const json = await res.json();
+        const getToken = async () => {
+            const response = await fetch('/auth/token');
+            const json = await response.json();
             setToken(json.access_token);
         }
         getToken();
-    }, [])
+    }, []);
 
-    const handleSubmit = (e) => {
-        // Send link to backend
-        fetch("http://localhost:8000/upload", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ link: url })
-        })
+    // const handleSubmit = (e) => {
+    //     // Send link to backend
+    //     fetch("http://localhost:8000/upload", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({ link: url })
+    //     })
 
-        // Receive data from backend
-        fetch("http://localhost:8000/", {
-            method: "GET",
-            headers: { "Accept": "application/json" }
-        }).then(
-            res => { 
-                return res.json();
-            }
-        ).then(
-            data => {
-                // Data contains the string from server
-                console.log(data.msg);
-            }
-        )
+    //     // Receive data from backend
+    //     fetch("http://localhost:8000/", {
+    //         method: "GET",
+    //         headers: { "Accept": "application/json" }
+    //     }).then(
+    //         res => { 
+    //             return res.json();
+    //         }
+    //     ).then(
+    //         data => {
+    //             // Data contains the string from server
+    //             console.log(data.msg);
+    //         }
+    //     )
         
-        e.preventDefault(); // To remove
-    }
+    //     e.preventDefault(); // To remove
+    // }
 
     return (
         <div className="music">
-            <form onSubmit={ handleSubmit }>
+            {/* <form onSubmit={ handleSubmit }>
                 <p>url:</p>
                 <input 
                     type="text" 
@@ -62,9 +62,9 @@ const Music = () => {
                 <button>YouTube</button>
                 <button>Spotify</button>
                 <button>SoundCloud</button>
-            </div>
+            </div> */}
 
-            { token !== "" ? <MusicPlayer image={ image } track_name="NIGHT RIDER" artist_name="Joji" /> : <SpotifyLogin token={ token }/> }
+            { (token === "") ? <SpotifyLogin /> : <SpotifyMusicPlayer token={ token }/> }
         </div>
     );
 }
