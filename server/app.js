1fdaa194257b6bef6d54d6b56928e781d5f8ee9b
carlxw <87335263+carlxw@users.noreply.github.com>
2023-03-29 17:25:42 -0400

    Remove request, replace with fetch

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const request = require("request");
+// const request = require("request");
 const dotenv = require("dotenv");
 
 const port = 8000
@@ -9,9 +9,11 @@ global.access_token = ""
 dotenv.config()
 let app = express();
 
-let spotify_client_id = process.env.SPOTIFY_CLIENT_ID
-let spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
-let spotify_redirect_uri = "http://localhost:3000/auth/callback"
+const config = require("../src/config.json");
+
+let spotify_client_id = "a4f3f6ce94e146d4aa6416ef2e859ea1"
+let spotify_client_secret = "e26d6398972a4bf0930f1c05907ce965"
+let spotify_redirect_uri = config.redirect_url;
 
 let generateRandomString = function (length) {
 	let text = "";
@@ -26,7 +28,7 @@ let generateRandomString = function (length) {
 /* ========== SPOTIFY AUTH ========== */
 
 app.get("/auth/login", (req, res) => {
-	let scope = "streaming user-read-email user-read-private"
+	let scope = "streaming \ user-read-email \ user-read-private"
 	let state = generateRandomString(16);
 
 	let auth_query_parameters = new URLSearchParams({
@@ -41,21 +43,21 @@ app.get("/auth/login", (req, res) => {
 })
 
 app.get("/auth/callback", async (req, res) => {
-	let code = req.query.code;
-
 	let authOptions = {
 		url: "https://accounts.spotify.com/api/token",
 		form: {
-			code: code,
+			code: req.query.code,
 			redirect_uri: spotify_redirect_uri,
 			grant_type: "authorization_code"
 		},
 		headers: {
 			"Authorization": "Basic " + (Buffer.from(spotify_client_id + ":" + spotify_client_secret).toString("base64")),
-			"Content-Type" : "application/x-www-form-urlencoded"
+			// "Content-Type" : "application/x-www-form-urlencoded"
 		}
 		// json: true
 	};
+
+	const form = new FormData
 
 	// request.post(authOptions.url, function(error, response, body) {
 	// 	if (!error && response.statusCode === 200) {
@@ -66,18 +68,16 @@ app.get("/auth/callback", async (req, res) => {
 
 	let response = await fetch(authOptions.url, {
 		method: "POST",
+		headers: authOptions.headers,
+		body: new URLSearchParams(authOptions.form)
 	})
 
-	console.log(response);
+	if (response.status === 200) {
+		const data = await response.json();
+		access_token = data.access_token;
 
-	// const data = await response.json();
-
-	// console.log(data);
-
-	// if (data.status === 200) {
-	// 	access_token = data.access_token;
-	// 	data.redirect("/");
-	// }
+		res.redirect("/");
+	}
 })
 
 app.get("/auth/token", (req, res) => {
